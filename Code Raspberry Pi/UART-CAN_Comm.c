583c58cb7a9107a3d8c91e2e13c568653fc00b78
Jeremie <jeremie.roy10@gmail.com>
2025-11-17 12:48:25 -0500

    .gitignore ajouté

--- Code Raspberry Pi/UART-CAN_Comm.c
+++ Code Raspberry Pi/UART-CAN_Comm.c
@@ -4,6 +4,7 @@
 *@author Jeremie Roy
 *@date 2025-11-14
 */
+#define _GNU_SURCE
 
 #include <stdio.h>
 #include <fcntl.h>   // File Control Definitions
@@ -13,10 +14,25 @@
 #include <string.h>
 #include <arpa/inet.h> // allows to send data to the net via the ehternet port
 #include <linux/spi/spidev.h>
+#include <sys/socket.h>
+
+const char *remote_address = "172.18.42.34";
 
 
 void main()
 {
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(5000);                    // destination port
+    inet_pton(AF_INET, "172.18.42.34", &addr.sin_addr); // destination IP
+
+    char message[] = "Hello over Ethernet!";
+    sendto(sock, message, strlen(message), 0,
+           (struct sockaddr*)&addr, sizeof(addr));
+
+    close(sock);
     //Ouverture des ports
 
     //fork()
